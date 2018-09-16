e52e0724894054fc1dff0c90942a32b2191892ef
vikas.hooda <vikas.hooda@keybenefit.com>
2018-09-16 18:27:53 +0530

    Xamarin Test app

---
+++ TestingAppX/TestingAppX/Views/Hack.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestingAppX.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Hack : ContentPage
+	{
+		public Hack ()
+		{
+			InitializeComponent ();
+
+            GetDefault();
+		}
+
+        private void GetDefault()
+        {
+
+            //Label label = new Label();
+            //label.Text = "http://www.google.com/";
+
+            //var tapGestureRecognizer = new TapGestureRecognizer();
+            //tapGestureRecognizer.Tapped += (s, e) => {
+            //    Device.OpenUri(new Uri(((Label)s).Text));
+            //};
+            //label.GestureRecognizers.Add(tapGestureRecognizer);
+
+            //Content = label;
+
+        }
+    }
+}
